62effb5b8c24917ba51cf9e5b5fc7aa87d73d421
twikssi <limpbiski15@gmail.com>
2020-09-13 15:46:30 +0300

    created User class, exception class UserValidateException and tests for these classes

---
+++ src/main/java/by/andrey/twikssi/exer1uservalidationservice/service/UserValidationService.java
@@ -0,0 +1,37 @@
+package by.andrey.twikssi.exer1uservalidationservice.service;
+
+import by.andrey.twikssi.exer1uservalidationservice.bean.User;
+import by.andrey.twikssi.exer1uservalidationservice.exceptions.UserValidationExceptiom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserValidationService {
+    public static Logger logger = LoggerFactory.getLogger(UserValidationService.class);
+
+    private UserValidationService() {
+    }
+
+    public static void getFirstNameWithValidate(User user, String name) throws UserValidationExceptiom {
+        if (name.length() >= 3 && name.length() < 15){
+            user.setFirstName(name);
+        } else {
+                throw new UserValidationExceptiom();
+        }
+    }
+
+    public static void getLastNameWithValidate(User user, String name) throws UserValidationExceptiom {
+        if (name.length() >= 3 && name.length() < 15){
+            user.setFirstName(name);
+        } else {
+            throw new UserValidationExceptiom();
+        }
+    }
+
+    public static void getAgeWithValidate(User user, int age) throws UserValidationExceptiom {
+        if (age >= 0 && age <= 120){
+            user.setAge(age);
+        } else {
+            throw new UserValidationExceptiom();
+        }
+    }
+}
