847b99ea2553879eb0be2cf6dd0a1416e7c1ee19
twikssi <limpbiski15@gmail.com>
2020-09-21 15:54:47 +0300

    created interface repository, class javabean book, exception ItemNotFoundException and service class Book repository

---
+++ src/main/java/by/andrey/twikssi/exer2simpledatabase/exceptions/ItemNotFoundException.java
@@ -0,0 +1,4 @@
+package by.andrey.twikssi.exer2simpledatabase.exceptions;
+
+public class ItemNotFoundException extends Exception{
+}
---
+++ src/main/java/by/andrey/twikssi/exer2simpledatabase/service/BookRepository.java
@@ -0,0 +1,4 @@
+package by.andrey.twikssi.exer2simpledatabase.service;
+
+public class BookRepository {
+}
